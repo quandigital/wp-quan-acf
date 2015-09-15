64e8f5ff021d8bf4e105f6569b3770f44ed7afb8
alpipego <alpipego@gmail.com>
2015-09-15 17:25:35 +0200

    basic plugin with acf json

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace QuanDigital\ACF\Plugin;
+namespace QuanDigital\ACF;
 
 use QuanDigital\WpLib\Boilerplate;
 
@@ -18,6 +18,16 @@ class Plugin extends Boilerplate
     private function hideAcf()
     {
         \add_filter('acf/settings/show_admin', '__return_false');
+    }
+
+    private function includeFields()
+    {
+        $this->path = \plugin_basename($this->file);
+        add_filter('acf/settings/load_json', function($paths) {
+            $paths[] = $this->getPath() . '/acf-json';
+            
+            return $paths;
+        }, 9);
     }
 
     private function saveJson() {
