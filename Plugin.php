b696b43b07027fb9555ec3bca01856793578e18f
alpipego <alpipego@gmail.com>
2015-09-22 17:24:22 +0200

    dont always include all fields (might lead to duplicates)

--- Plugin.php
+++ Plugin.php
@@ -20,16 +20,6 @@ class Plugin extends Boilerplate
         \add_filter('acf/settings/show_admin', '__return_false');
     }
 
-    private function includeFields()
-    {
-        $this->path = \plugin_basename($this->file);
-        add_filter('acf/settings/load_json', function($paths) {
-            $paths[] = $this->getPath() . '/acf-json';
-            
-            return $paths;
-        }, 9);
-    }
-
     private function saveJson() {
         add_filter('acf/settings/save_json', function() {
             return $this->getPath() . '/acf-json';
