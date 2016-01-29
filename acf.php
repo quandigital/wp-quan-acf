21f6fd25bbcfe182a40241e87a7aebebebb77260
alpipego <alpipego@gmail.com>
2016-01-29 08:47:39 +0100

    references acf changed, v 1.0.7

--- acf.php
+++ acf.php
@@ -8,7 +8,7 @@ namespace QuanDigital\ACF;
  * Author: Quan Digital GmbH
  * Author URI: http://www.quandigital.com/
  * Description: General ACF Settings
- * Version: 1.0.6
+ * Version: 1.0.7
  * License: MIT
  */
 
